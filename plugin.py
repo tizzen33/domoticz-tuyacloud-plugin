d0c7bc4e1fbc5eb329433173f3ab82d2858f7171
tizzen33 <53906250+tizzen33@users.noreply.github.com>
2020-01-24 18:56:33 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -143,9 +143,9 @@ class BasePlugin:
                 Domoticz.Debug('Tuya state: ' + str(response_json["payload"]["data"]["state"]))
                 Domoticz.Debug('Domoticz state: ' + str(Devices[Unit].nValue))
                 Domoticz.Debug('State value true: ' + str(states["true"]) + ' , false: ' + str(states["false"]))
-                if(Devices[Unit].nValue != states[str(response_json["payload"]["data"]["state"])]):
-                    Devices[Unit].Update(nValue = states[str(response_json["payload"]["data"]["state"])], sValue = str(states[str(response_json["payload"]["data"]["state"])]))
-                    Domoticz.Debug('Device ' + Devices[Unit].Name + ' status updated to ' + str(response_json["payload"]["data"]["state"]))
+                #if(Devices[Unit].nValue != states[str(response_json["payload"]["data"]["state"])]):
+                #    Devices[Unit].Update(nValue = states[str(response_json["payload"]["data"]["state"])], sValue = str(states[str(response_json["payload"]["data"]["state"])]))
+                #    Domoticz.Debug('Device ' + Devices[Unit].Name + ' status updated to ' + str(response_json["payload"]["data"]["state"]))
 
             
     def onStop(self):
