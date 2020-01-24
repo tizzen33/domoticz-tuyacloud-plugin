8f9d3faaae51bd80c4cbf7851e22d9bf9026994b
tizzen33 <53906250+tizzen33@users.noreply.github.com>
2020-01-24 23:05:42 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -145,7 +145,7 @@ class BasePlugin:
                 Domoticz.Debug('State value true: ' + str(states["True"]) + ' , false: ' + str(states["False"]))
                 if(Devices[Unit].nValue != states[str(response_json["payload"]["data"]["state"])]):
                 #    Devices[Unit].Update(nValue = states[str(response_json["payload"]["data"]["state"])], sValue = str(states[str(response_json["payload"]["data"]["state"])]))
-                #    Domoticz.Debug('Device ' + Devices[Unit].Name + ' status updated to ' + str(response_json["payload"]["data"]["state"]))
+                    Domoticz.Debug('Device ' + Devices[Unit].Name + ' status updated to ' + str(response_json["payload"]["data"]["state"]))
 
             
     def onStop(self):
